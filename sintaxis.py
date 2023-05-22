7b7516adc1362ef4b23ed6e1e3ea786c6aa7b400
Yeltsin7 <Yelsinlov@gmail.com>
2023-05-22 12:16:42 -0400

    Variables y tipos de datos

---
+++ sintaxis.py
@@ -0,0 +1,10 @@
+#Sintaxis Basica de Python
+#VARIABLES
+materia="OBJETOS I"
+print(materia)
+#TIPOS DE DATOS
+#DATOS SIMPLES
+#1. TIPOS DE DATOS NUMERICOS
+numero=445
+print(numero)
+#DATOS COMPUESTOS
