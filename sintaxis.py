96702614eb76f197134f002ed2453e3d88cf116d
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-05-22 12:10:34 -0400

    variables y tipos de datos

---
+++ sintaxis.py
@@ -0,0 +1,11 @@
+#Sintaxis basica de Python
+#VARIABLES
+materia="Objetos I"
+print(materia)
+#TIPOS DE DATOS
+#DATOS SIMPLES
+#1. TIPOS DE DATOS NUMERICOS
+print("1. TIPOS DE DATOS NUMERICOS")
+numero=445
+print(numero)
+#DATOS COMPUESTOS
