d14f90df3c833db21074edc58957ff28d528e9a2
romernina <romerninap@gmail.com>
2023-05-22 12:05:35 -0400

    modificacion de datos en sintaxis.py

---
+++ sintaxis.py
@@ -0,0 +1,9 @@
+materia="OBJETOS I"
+print(materia)
+#tipos de datos
+#datos simples
+#1 tipos de datos numericos
+print("1 Tipos de datos numericos")
+numero=45,45
+print(numero)
+#datos compuestos
