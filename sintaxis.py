416621621e5c409c8e1ef82c411e8939b8f8f342
Limberth01 <125583065+Limberth01@users.noreply.github.com>
2023-05-22 12:11:56 -0400

    variables y tipos de datos

---
+++ sintaxis.py
@@ -0,0 +1,9 @@
+#sintaxis basicas de python
+#variables 
+materia="objetos I"
+print(materia)
+#tipos de datos simples y compuestos
+#1. datos numericos
+print("1. tipos de datos numericos")
+numero=445
+print(numero)
