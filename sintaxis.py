a2e606c7b65380f428ea9ef7ad3a27e50e747360
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-05-23 11:49:53 -0400

    Tipos de datos Python

--- sintaxis.py
+++ sintaxis.py
@@ -5,11 +5,7 @@ print(materia)
 #TIPOS DE DATOS
 #DATOS SIMPLES
 #1. TIPOS DE DATOS NUMERICOS
-<<<<<<< HEAD
 print("1. TIPOS DE DATOS NUMERICOS")
 numero=44567
-=======
-numero=445
->>>>>>> 7b7516adc1362ef4b23ed6e1e3ea786c6aa7b400
 print(numero)
 #DATOS COMPUESTOS
