37f1c34b5d2050d8853bd8e0fd9f490c79881719
Yeltsin7 <Yelsinlov@gmail.com>
2023-05-23 11:49:46 -0400

    Tipos de datos en Python Y

---
+++ datos.py
@@ -0,0 +1,45 @@
+#TIPOS DE DATOS EN PYTHON
+#1.  Tipos de datos Simples
+#1.1. Tipos de datos numericos
+print("Tipos de datos en Python")
+print("1. Tipos de datos simples")
+print("1.1. Tipos de datos numericos")
+numero1=75
+numero2=15.1248
+print(numero1)
+print(numero2)
+
+#1.2. Tipos de datos de cadena de Caracteres
+print("1.2. Tipos de datos de cadena de Caracteres")
+materia="Objetos I - Catec"
+carrera="Sistemas Informaticos - 2023"
+edad="24"
+simbolo="@"
+print(materia)
+print(carrera)
+print(edad)
+print(simbolo)
+
+#1.3. Tipos de datos boleanos
+print("1.3. Tipos de datos boleanos")
+enceder=True
+apagar=False
+
+#2 Tipos de datos Compuestos
+print("2 Tipos de datos Compuestos")
+
+#2.1. Tipos de datos listas
+print("2.1. Tipos de datos listas")
+colores=["rojo","verde","azul","violeta"]
+print(colores[3])
+print(colores[2])
+
+#2.2. Tipos de datos Tuplas
+print("2.2. Tipos de datos Tuplas")
+nombre=('Yel','Alberto','Nayeli','Ramiro','Kevin',True)
+print(nombre[0])
+
+#2.3. Tipos de datos Diccionarios
+print("2.3. Tipos de datos Diccionarios")
+paises={"nombre":"Bolivia","NumDepartamentos":9,"NumHabitantes":"12.08 millones","Capital":"Sucre"}
+print(paises["nombre"], paises["NumHabitantes"])
