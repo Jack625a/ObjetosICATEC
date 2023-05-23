0b1a1ca7423a7faec2b4b56b5e8a3566b8a727b0
Adrian Ortuño Alandia <adriortuala@gmail.com>
2023-05-23 11:50:57 -0400

    Datos Python

---
+++ Dato.py
@@ -0,0 +1,44 @@
+#Tipos de Datos en Python
+#1. Tipos de Datos Simples
+#1.1. Tipos de Datos Numerico
+print("Tipos de Datos en Python")
+print("1. Tipos de Datos Simples")
+print("1.1. Tipos de Datos Numerico")
+num1=75
+num2=15.1248
+print(num1)
+print(num2)
+
+#1.2. Tipos de Datos Cadena de Caracteres
+print("1.2. Tipos de Datos Cadena de Caracteres")
+carre="Sistemas Informaticos - Catec"
+mater="Objetos 1 - 2023"
+edad="27"
+print(carre)
+print(mater)
+print(edad)
+
+#1.3. Tipos de Datos Boleanos
+print("1.3. Tipos de Datos Boleanos")
+Encender=True
+Apagado=False
+
+#2. Tipos de Datos Compuestos 
+#2.1. Tipos de Datos Listas
+print("2. Tipos de Datos Compuestos")
+print("2.1. Tipos de Datos Listas")
+Colores=["Rojo", "Verde", "Cafe", "Azul", "Rosa"]
+print(Colores)
+print(Colores[2])
+
+#2.2. Tipos de Datos Tuplas
+print("2.2. Tipos de Datos Tuplas")
+Nombres=("Kevin", 45, "Ramiro", "Yeltsin", "Alberto", "Nayeli")
+print(Nombres)
+print(Nombres[5])
+
+#2.3. Tipos de Datos Diccionario
+print("2.3. Tipos de Datos Diccionario")
+Paises={"Nombre":"Bolivia", "Numero de Departamentos":9, "Numero de Habitantes":"12.08 millones","Capital":'Sucre'}
+print(Paises)
+print(Paises["Nombre"])
