8a27d2e3b5c17d2d64bb500e5c0230b837052439
SK-NAYELI-978 <129773922+SK-NAYELI-978@users.noreply.github.com>
2023-05-23 12:08:38 -0400

    tipos de datos

---
+++ datos.py
@@ -0,0 +1,37 @@
+#Tipos de datos en Python
+#1. Tipos de datos Simples
+#1.1. Tipos de datos numericos
+print("Tipos de datos en Python")
+print("1. Tipos de datos Simples")
+print("1.1. Tipos de datos numericos")
+numero1=75
+numero2=15.1248
+print(numero1)
+print(numero2)
+#1.2. Tipos de datos de Cadena de Caracteres
+print("1.2. Tipos de datos de Cadena de Caracteres")
+materia="Objetos I - Catec"
+carrera="Sistemas Informaticos - 2023"
+edad='27'
+simbolos="@►X|♪ù▒▒Y"
+print(materia)
+print(simbolos)
+#1.3. Tipos de datos Boleanos
+print("1.3. Tipos de datos Boleanos")
+encender=True
+apagar=False
+
+#2. Tipos de datos Compuestos
+#2.1. Tipos de datos Listas
+print("2. Tipos de datos Compuestos")
+print("2.1. Tipos de datos Listas")
+colores=["rojo","verde","cafe","azul","rosado"]
+print(colores[4])
+#2.2. Tipos de datos Tuplas
+print("2.2. Tipos de datos Tuplas")
+nombre=("Kevin",45,"Ramiro","Yeltsin","Alfredo","Alberto","Nayeli",True)
+print(nombre[0])
+#2.3. Tipos de datos Diccionarios
+print("2.3. Tipos de datos Diccionarios")
+paises={"Nombre":"Bolivia","NumDepartamentos":9,"NumHabitantes":"12.08 millones","Capital":"Sucre"}
+print(paises["NumDepartamentos"])
