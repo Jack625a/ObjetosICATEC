ebc4cd20035e82265732f50ac591a6bf7c54ba77
romernina <romerninap@gmail.com>
2023-05-23 11:33:57 -0400

    creacion de datos.py

---
+++ datos.py
@@ -0,0 +1,34 @@
+#tipos de datos en python
+#1 tipos de datos simples
+#1.1. tipos de datos numericos
+print("TIPOS DE DATOS NUMERICOS")
+numero1=75
+numero2=15.1248
+print(numero1,"welcome")
+print(numero2)
+print(numero1*numero2)
+#1.1. tipos de datos string
+print("TIPOS DE DATOS STRING")
+materia="OBJETOS I"
+carrera='SISTEMAS INFORMATICOS-2023'
+edad='10'
+print(materia)
+num=int(edad)
+print(num)
+simbolos="@#%&/ª"
+print(simbolos)
+#1.3. tipos de datos booleanos
+print("TIPOS DE DATOS BOOLEANOS")
+encender=True
+apagar=False
+print(encender)
+
+#2 tipso de datos compuestos
+#2.1. tipos de datos listas
+print("TIPOS DE DATOS LISTAS")
+colores=["rojo","verde","cafe","azul","rosado"]
+print(colores[2])
+
+#2.2. tipos de datos tuplas
+#2.3. tipos de datos diccionarios
+
