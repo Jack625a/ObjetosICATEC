c24b8a9f9523a64dd37ba76572a611dd355f5785
romernina <romerninap@gmail.com>
2023-05-30 07:42:03 -0400

    funciones 1

---
+++ funciones.py
@@ -0,0 +1,10 @@
+#funcion sumar
+def sumar(num1,num2):
+    resultado=num1+num2
+    return resultado
+a=float(input("ingrese num con decimal: "))
+b=float(input("ingrese num con decimal: "))
+print(sumar(a,b))
+print(sumar(4.6,5.4))
+
+
