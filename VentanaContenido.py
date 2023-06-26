ed3f097adf79b0b0d36cfef2924b098a789ba7d4
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-06-26 11:00:10 -0400

    Ventana Generarcion Contenido

---
+++ VentanaContenido.py
@@ -0,0 +1,21 @@
+#Importar las librerias 
+import customtkinter as ctk 
+import tkinter
+import openai
+from PIL import Image, ImageTk
+
+openai.api_key="API KEY" #REEMPLAZAR POR SU API KEY
+def obtener_respuesta(pregunta):
+    respuesta=openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=pregunta,
+        temperature=0.5,
+        max_tokens=200,
+    )
+    return respuesta.choices[0].text.strip()
+
+def generar_respuesta():
+    pregunta=entrada_prompt.get("0.0",tkinter.END)
+    respuesta=obtener_respuesta(pregunta)
+    respuesta_text.delete("0.0",tkinter.END)
+    respuesta_text.insert(tkinter.END,respuesta)
