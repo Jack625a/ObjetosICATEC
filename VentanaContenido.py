cbdfcd4e6d512bfd834df8ccc081ac9745f429d3
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-06-26 11:30:23 -0400

    Ventana Generacion de Contenido

--- VentanaContenido.py
+++ VentanaContenido.py
@@ -19,3 +19,36 @@ def generar_respuesta():
     respuesta=obtener_respuesta(pregunta)
     respuesta_text.delete("0.0",tkinter.END)
     respuesta_text.insert(tkinter.END,respuesta)
+
+ventana=ctk.CTk()
+ventana.title("Aprende a Programar - CATEC")
+ctk.set_appearance_mode("dark")
+pantalla=ctk.CTkFrame(ventana)
+pantalla.pack(side="left",padx=20,pady=20)
+
+prompt_label=ctk.CTkLabel(pantalla,text="Ingrese su pregunta.")
+prompt_label.grid(row=0,column=0,padx=10,pady=10)
+entrada_prompt=ctk.CTkTextbox(pantalla,height=10)
+entrada_prompt.grid(row=0,column=1,padx=10,pady=10)
+
+#Boton para el envio del prompt al servicio del modelo
+boton=ctk.CTkButton(pantalla,text="Realizar Pregunta", command=generar_respuesta )
+boton.grid(row=1,column=0,padx=10,pady=10)
+
+#MOSTRAR EN PANTALLA LA RESPUESTA OBTENIDA POR EL MODELO
+respuesta_label=ctk.CTkLabel(pantalla,text="Respuesta")
+respuesta_label.grid(row=2,column=0,padx=10,pady=10)
+respuesta_text=ctk.CTkTextbox(pantalla,height=10,state="disabled")
+respuesta_text.grid(row=2,column=1,padx=10,pady=10)
+
+canvas=tkinter.Canvas(ventana,width=512, height=512)
+canvas.pack(side="left")
+
+#Insercion de la imagen
+imagen=Image.open("aprender.jpg")
+imagen=imagen.resize((512,512))
+imagen_mostrada=ImageTk.PhotoImage(imagen)
+imagen_label=tkinter.Label(canvas,image=imagen_mostrada)
+imagen_label.pack()
+
+ventana.mainloop()
